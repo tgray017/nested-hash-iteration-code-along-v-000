7b9d05c6668066a4549dc90f3b8a62e7d15638b4
tgray017 <tgray017@gmail.com>
2018-10-05 23:02:04 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,6 +17,7 @@ require 'pry'
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     contact_details_hash.each do |attribute, data|
+      binding.pry
       if attribute == :favorite_ice_cream_flavors
         attribute.each do |flavor|
           attribute.delete(flavor) if flavor == "strawberry"
