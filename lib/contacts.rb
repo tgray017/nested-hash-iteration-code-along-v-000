d969ce8dec803997104812b4c13b9e461309b925
tgray017 <tgray017@gmail.com>
2018-10-05 22:54:13 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,15 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-  contacts.each do 
+  contacts.each do |name, data|
+    if name == "Freddy Mercury"
+      data.each do |attribute, value|
+        if attribute == :favorite_ice_cream_flavors
+          attribute.each do |flavor|
+            if flavor == 
+          end
+        end
+      end
+    end
+  end
 end
